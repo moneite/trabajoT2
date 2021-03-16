ab80b6e2a7f673330502de9263e2d183fd93b83e
Miguel Angel Arcos <moneite@gmail.com>
2021-03-16 16:30:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# trabajoT2
+ Trabajo Integrador del Segundo Trimestre - Miguel Angel Arcos Reyes
---
+++ js/index.js
@@ -0,0 +1,12 @@
+actualizar();
+
+function actualizar() {
+    if (sessionStorage.contador) {
+        sessionStorage.contador = Number(sessionStorage.contador) + 1;
+    } else {
+        sessionStorage.setItem("contador", 1);
+    }
+    $("#alerta").text('Has visitado ' + sessionStorage.contador + ' páginas.')
+}
+
+$("#bc").text("Inicio");
